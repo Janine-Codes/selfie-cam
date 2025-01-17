e25219b76a9b2520199f4caef42708a6fda373cb
Janine-Codes <janinekrse@gmail.com>
2025-01-17 15:33:33 +0100

    Moved esling.config.js out of .gitignore

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import pluginJs from "@eslint/js";
+import prettierConfig from "eslint-config-prettier";
+import prettier from "eslint-plugin-prettier";
+import globals from "globals";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  prettierConfig,
+  {
+    plugins: {
+      prettier: prettier,
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+];
